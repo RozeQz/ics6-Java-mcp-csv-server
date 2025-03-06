0728d6a959774fa257e37c7f739049a22d58016e
RozeQz <liza.roz@yandex.ru>
2025-03-06 12:16:44 +0300

    Initial commit

---
+++ src/main/java/ru/rozhkova/rk1/Rk1Application.java
@@ -0,0 +1,13 @@
+package ru.rozhkova.rk1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rk1Application.class, args);
+	}
+
+}
